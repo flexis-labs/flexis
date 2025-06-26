1a8145bc45b8f4c5cf0a389e0f793a1ac554b0db
Dmitry <61691589+flexis-labs@users.noreply.github.com>
2025-06-26 15:33:06 +1000

    Update Russian.php

--- src/language/Stemmer/Russian.php
+++ src/language/Stemmer/Russian.php
@@ -94,6 +94,88 @@ class Russian implements StemmerInterface
         return $word;
     }
 
-    // Остальные методы
-    // ...
+    /**
+     * Заменяет первую строку второй, находящейся в конце строки. 
+     * Если указан третий аргумент, то предыдущая строка должна соответствовать как минимум этому числу m.
+     *
+     * @param   string        $str    Строка для проверки.
+     * @param   string        $check  Окончание проверки.
+     * @param   string        $repl   Запасная строка.
+     * @param   integer|null  $m      Необязательное минимальное количество m() для удовлетворения.
+     *
+     * @return  boolean  Находится ли строка $check в конце строки $str. Правда не обязательно означает, что его заменили.
+     */
+    private function replace(string &$str, string $check, string $repl, ?int $m = null): bool {
+        $len = 0 - \strlen($check);
+
+        if (substr($str, $len) == $check) {
+            $substr = substr($str, 0, $len);
+
+            if ($m === null || $this->m($substr) > $m) {
+                $str = $substr . $repl;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * m() измеряет количество последовательностей согласных в $str. 
+     * если c — последовательность согласных, 
+     * а v — последовательность гласных, 
+     * а <..> указывает на произвольное присутствие,
+     *
+     * <c><v>       gives 0
+     * <c>vc<v>     gives 1
+     * <c>vcvc<v>   gives 2
+     * <c>vcvcvc<v> gives 3
+     *
+     * @param   string  $str  Строка, возвращающая количество m.
+     *
+     * @return  integer  Количество m.
+     */
+    private function m(string $str): int {
+        $c = $this->regexConsonant;
+        $v = $this->regexVowel;
+
+        $str = preg_replace("#^$c+#", '', $str);
+        $str = preg_replace("#$v+$#", '', $str);
+
+        preg_match_all("#($v+$c+)#", $str, $matches);
+
+        return \count($matches[1]);
+    }
+
+    /**
+     * Возвращает значение true/false в зависимости от того, содержит ли данная строка две одинаковые согласные рядом друг с другом в конце строки.
+     *
+     * @param   string  $str  Строка для проверки.
+     *
+     * @return  boolean  Результат.
+     */
+    private function doubleConsonant(string $str): bool {
+        $c = $this->regexConsonant;
+
+        return preg_match("#$c{2}$#", $str, $matches) && $matches[0][0] === $matches[0][1];
+    }
+
+    /**
+     * Проверяет окончание последовательности CVC, где второй C не является W, X или Y.
+     *
+     * @param   string  $str  Строка для проверки.
+     *
+     * @return  boolean  Результат.
+     */
+    private function cvc(string $str): bool {
+        $c = $this->regexConsonant;
+        $v = $this->regexVowel;
+
+        return preg_match("#($c$v$c)$#", $str, $matches)
+            && \strlen($matches[1]) === 3
+            && $matches[1][2] !== 'w'
+            && $matches[1][2] !== 'x'
+            && $matches[1][2] !== 'y';
+    }
 }
